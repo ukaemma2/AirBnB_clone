04995293c905c526d3b188cfcb7d36cf75506296
Ukah Emmanuel Ogbonna <48858163+ukaemma2@users.noreply.github.com>
2023-03-10 14:22:43 +0100

    save_basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -28,6 +28,27 @@ class BaseModel:
                 if key == 'created_at' or key == 'updated_at':
                     self.__dict__[key] = datetime.strptime(value, time_format)
                 else:
-                    self.__dict__[key] = v
+                    self.__dict__[key] = value
         else:
             models.storage.new(self)
+
+    def save(self):
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
+        models.storage.save()
+
+    def to_dict(self):
+        """Return the dictionary of the BaseModel instance.
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict['created_at'] = self.updated_at.isoformat()
+        rdict['__class__'] = self.__class__.__name__
+        return rdict
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        class_name = self.__class__.__name__
+        return '[{}] ({}) {}'.format(class_name, self.id, self.__dict__)
