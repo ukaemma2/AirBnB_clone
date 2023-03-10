e4a099f41cceaf630167d31db382a5f10f596b0c
Ukah Emmanuel Ogbonna <48858163+ukaemma2@users.noreply.github.com>
2023-03-10 15:39:17 +0100

    module models has no attribute storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
-
-"""Bsae model that define all common
-    attributes/method for other class
-"""
-
+"""Defines the BaseModel class."""
 import models
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """represents the BaseModel of the HBnB project."""
+    """Represents the BaseModel of the HBnB project."""
 
-    def __int__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel.
         Args:
             *args (any): Unused.
-            **kwargs (dict): key/value pairs of attributs.
+            **kwargs (dict): Key/value pairs of attributes.
         """
-
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    self.__dict__[key] = datetime.strptime(value, time_format)
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
                 else:
-                    self.__dict__[key] = value
+                    self.__dict__[k] = v
         else:
             models.storage.new(self)
 
@@ -44,11 +39,11 @@ class BaseModel:
         """
         rdict = self.__dict__.copy()
         rdict["created_at"] = self.created_at.isoformat()
-        rdict['created_at'] = self.updated_at.isoformat()
-        rdict['__class__'] = self.__class__.__name__
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
         return rdict
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
-        class_name = self.__class__.__name__
-        return '[{}] ({}) {}'.format(class_name, self.id, self.__dict__)
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
