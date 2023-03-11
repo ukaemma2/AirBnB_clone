e57deac2e287e81b325c0496e80bb3dd4dfcf356
Ukah Emmanuel Ogbonna <48858163+ukaemma2@users.noreply.github.com>
2023-03-11 10:03:59 +0100

    tested

--- models/base_model.py
+++ models/base_model.py
@@ -14,16 +14,16 @@ class BaseModel:
             *args (any): Unused.
             **kwargs (dict): Key/value pairs of attributes.
         """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        time_fomat = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
+            for key, value in kwargs.items():
+                if key() == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, time_fomat)
                 else:
-                    self.__dict__[k] = v
+                    self.__dict__[key] = value
         else:
             models.storage.new(self)
 
@@ -37,11 +37,11 @@ class BaseModel:
         Includes the key/value pair __class__ representing
         the class name of the object.
         """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+        returned_dict = self.__dict__.copy()
+        returned_dict["created_at"] = self.created_at.isoformat()
+        returned_dict["updated_at"] = self.updated_at.isoformat()
+        returned_dict["__class__"] = self.__class__.__name__
+        return returned_dict
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
