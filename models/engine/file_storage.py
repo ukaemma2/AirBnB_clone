6bc149aea96b58522669fb746ef773371926d417
ukah emmanuel ogbonna <ukaemmanuel2000@gmail.com>
2023-03-22 23:19:04 +0100

    __pycach__

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -46,5 +46,5 @@ class FileStorage:
                     cls_name = o["__class__"]
                     del o["__class__"]
                     self.new(eval(cls_name)(**o))
-        except FileNotFoundError:
+        except  FileNotFoundError:
             return
