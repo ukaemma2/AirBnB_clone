2fec24733fd0bdf4edc2e6be09b284e0ba6a4f1e
ukah emmanuel ogbonna <ukaemmanuel2000@gmail.com>
2023-04-03 21:33:46 +0100

    more changes

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines the BaseModel class."""
-# import models
+import models
 from uuid import uuid4
 from datetime import datetime
 
@@ -14,23 +14,23 @@ class BaseModel:
             *args (any): Unused.
             **kwargs (dict): Key/value pairs of attributes.
         """
-        # time_fomat = "%Y-%m-%dT%H:%M:%S.%f"
+        time_fomat = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
-        # if len(kwargs) != 0:
-        #     for key, value in kwargs.items():
-        #         if key == "created_at" or key == "updated_at":
-        #             self.__dict__[key] = datetime.strptime(value, time_fomat)
-        #         else:
-        #             self.__dict__[key] = value
-        # else:
-        #     models.storage.new(self)
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, time_fomat)
+                else:
+                    self.__dict__[key] = value
+        else:
+            models.storage.new(self)
 
     def save(self):
         """Update updated_at with the current datetime."""
         self.updated_at = datetime.today()
-        # models.storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Return the dictionary of the BaseModel instance.
