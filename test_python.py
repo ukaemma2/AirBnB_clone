300a6ac69656159062c50762a3006d66bbee07b8
ukah emmanuel ogbonna <ukaemmanuel2000@gmail.com>
2023-05-19 20:51:43 +0100

    changes

--- test_python.py
+++ test_python.py
@@ -1,5 +1,3 @@
-students = ['Chika', 'peterAjah', 'Biola_Lawal', 'Emmanuel', 'Ukason']
 
-# print the students and the character numbers
-for student in students:
-    print (student, len(student))
+class MyEmptyClass:
+    pass
