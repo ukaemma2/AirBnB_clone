6af37d163f52ff46ed03c6886b623a0b07cd02d9
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-08-20 18:59:45 +0000

    changes

--- console.py
+++ console.py
@@ -156,11 +156,13 @@ class HBNBCommand(cmd.Cmd):
         print(count)
 
     def do_update(self, arg):
+
         """Usage: update <class> <id> <attribute_name> <attribute_value> or
        <class>.update(<id>, <attribute_name>, <attribute_value>) or
        <class>.update(<id>, <dictionary>)
         Update a class instance of a given id by adding or updating
         a given attribute key/value pair or dictionary."""
+
         argl = parse(arg)
         objdict = storage.all()
 
